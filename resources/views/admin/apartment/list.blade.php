f50c0867692b6302e307b9187a592f7e6262779b
Giang Thị Tình <tinhgiang9898@gmail.com>
2018-08-06 17:08:19 +0700

    'nop'

---
+++ resources/views/admin/apartment/list.blade.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Danh sách chung cư</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css"
+          integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/list.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+<nav>
+    <ul class="menu">
+        <li><a href="/admin/apartment">Home</a></li>
+        <li><a href="/admin/apartment">Best Home</a></li>
+        <li><a href="/admin/apartment">Life style</a></li>
+        <li><a href="/admin/apartment">About us</a></li>
+        <li><a href="/admin/apartment">Contact us</a></li>
+    </ul>
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="card col-4">
+            <img class="card-img-top" src=".../100px180/">
+            <div class="card-body">
+                <h5 class="card-title">Home 1</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+        </div>
+        <div class="card col-4">
+            <img class="card-img-top" src=".../100px180/">
+            <div class="card-body">
+                <h5 class="card-title">Home 1</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+        </div>
+        <div class="card col-4">
+            <img class="card-img-top" src=".../100px180/">
+            <div class="card-body">
+                <h5 class="card-title">Home 1</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="card col-4">
+            <img class="card-img-top" src=".../100px180/">
+            <div class="card-body">
+                <h5 class="card-title">Home 1</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+        </div>
+        <div class="card col-4">
+            <img class="card-img-top" src=".../100px180/">
+            <div class="card-body">
+                <h5 class="card-title">Home 1</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+        </div>
+        <div class="card col-4">
+            <img class="card-img-top" src=".../100px180/">
+            <div class="card-body">
+                <h5 class="card-title">Home 1</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
